3ee43ac1d9765ca4fba77d6896bc1362eb6c81e8
Cooper Stimson <cooperstimson@gmail.com>
2014-10-01 14:54:48 -0700

    Update kickme.c

--- c_interpreter/kickme.c
+++ c_interpreter/kickme.c
@@ -4,7 +4,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char ** argv)
+main(int argc, char *argv[])
 {
-  /* @todo write this */
+  FILE *fp;
+  char buff[255];
+
+  if ( argc != 2 )
+  {
+        printf( "usage: %s foo.km", argv[0] );
+  } else {
+        
+    /* Open file */
+    fp = fopen(argv[1], "r");
+    if( fp != NULL ){
+      while ( !feof(fp ) ){
+        memset(buff, '\0', sizeof( buff) );
+        fscanf(fp, "%s", &buff);
+        printf("%s\n", buff );
+      }
+            
+      /* Close file */
+      fclose(fp);
+    } else {
+      printf("Could not open file '%s'", argv[1]);
+            
+    }
+  }
 }
