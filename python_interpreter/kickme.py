1bd54f83d2b9f71c481ef1a346743879c2b048d2
Cooper Stimson <cooperstimson@gmail.com>
2014-10-01 16:00:32 -0700

    Update kickme.py

--- python_interpreter/kickme.py
+++ python_interpreter/kickme.py
@@ -1,3 +1,12 @@
 import string
+import argparse
 
 # @todo write this
+
+def main.py():
+	
+	# Parse cli arguments
+	parser = argparse.ArgumentParser()
+	parser.parse_args()
+	
+if __name__=="__main__": main()
